14fc22a37de6120d1e054e9e110a3de7445c8091
huyntrang1 <htrangforw@gmail.com>
2023-11-04 01:48:12 +0700

    init automation framework

---
+++ testdata/com/nopcommerce/data/UserData.java
@@ -0,0 +1,19 @@
+package com.nopcommerce.data;
+
+public class UserData {
+	public class Register {
+
+	}
+
+	public class Login {
+
+	}
+
+	public class Adress {
+
+	}
+
+	public class MyAccount {
+
+	}
+}
